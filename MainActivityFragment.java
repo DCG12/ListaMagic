6c525167aa364ea46b9f7a462e7202979c6f17c7
David <46406163y@iespoblenou.org>
2016-10-18 17:37:43 +0200

    Afegir Listview, layaout amb el nom de les cartes i el arrayadapter

--- MainActivityFragment.java
+++ MainActivityFragment.java
@@ -6,11 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     private int num;
 
@@ -24,6 +29,15 @@ public class MainActivityFragment extends Fragment {
          View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvCards = (ListView) view.findViewById(R.id.lvCards);
+
+        items = new ArrayList<>();
+               adapter = new ArrayAdapter<>(
+                               getContext(),
+                               R.layout.ls_cards_rows,
+                               R.id.txtNameCard,
+                              items
+                           );
+                lvCards.setAdapter(adapter);
 
         return view;
     }
