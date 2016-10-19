fb712f07a6bf83d51cd4b1ddb36f109c4dceac1d
David <46406163y@iespoblenou.org>
2016-10-19 19:34:03 +0200

    Inflem el menu de cartes i fem refresh

--- MainActivityFragment.java
+++ MainActivityFragment.java
@@ -9,6 +9,10 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -22,6 +26,14 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
+
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+
+                        setHasOptionsMenu(true);
+           }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,4 +65,21 @@ public class MainActivityFragment extends Fragment {
 
         return view;
     }
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+                super.onCreateOptionsMenu(menu, inflater);
+                inflater.inflate(R.menu.menu_cartes_fragment, menu);
+            }
+
+    @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+                                        int id = item.getItemId();
+                                if (id == R.id.action_refresh) {
+            refresh();
+                        return true;
+                   }
+                        return super.onOptionsItemSelected(item);
+            }
+                private void refresh() {
+            }
 }
